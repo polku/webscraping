92b2e434a417b5a6ac4abe395ec4d8d3c4e4fe1f
charon <charon@deb>
2014-06-13 18:01:28 +0200

    +heure, et parcours pages auto

--- scrapy/ppm/ppm/items.py
+++ scrapy/ppm/ppm/items.py
@@ -1,9 +1,10 @@
 from scrapy.item import Item, Field
 
 class PpmItem(Item):
+    prix = Field()
+    hour = Field()
     age = Field()
     qtm = Field()
     lc	= Field()
     xp	= Field()
     tc	= Field()
-    prix = Field()
--- scrapy/ppm/ppm/spiders/test.py
+++ scrapy/ppm/ppm/spiders/test.py
@@ -6,13 +6,7 @@ from ppm.items		import PpmItem
 class	PpmSpider(Spider):
 	name = "ppm"
 	allowed_domains = ["soccer.powerplaymanager.com"]
-	start_urls = [
-			"http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday",
-			"http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday-2",
-			"http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday-3",
-			"http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday-4",
-			"http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday-5",
-			]
+	start_urls = ["http://soccer.powerplaymanager.com/en/most-expensive.html?data=lastday-%d" %(n) for n in range(1,50)]
 
 	def parse(self, response):
 		sel = Selector(response)
@@ -20,11 +14,12 @@ class	PpmSpider(Spider):
                 items = []
 		for p in plyr:
                     item = PpmItem()
+                    item['hour'] = p.xpath('td[@class="left_align"]/div[@align="center"]/text()').re(r'\d\d:\d\d:\d\d')
+                    item['prix'] = p.xpath('td[@class="left_align"]/div/text()').re(r'(?<=Price: ).*')
                     item['age'] = p.xpath('td[@title="Age"]/text()').extract()
                     item['qtm'] = p.xpath('td[@title="Average quality"]/text()').extract()
                     item['lc']	= p.xpath('td[@title="Career longevity"]/span/text()').extract()
                     item['xp']	= p.xpath('td[@title="Experience"]/text()').extract()
                     item['tc']	= p.xpath('td[@title="Overall rating"]/text()').extract()
-                    item['prix'] = p.xpath('td[@class="left_align"]/div/text()').re(r'(?<=Price: ).*')
                     items.append(item)
 		return items
